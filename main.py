384776ed264dbc50baba56961bbb492aecc739d2
Marwan Mostafa <marwan.d.2008@gmail.com>
2024-06-30 12:23:04 +0300

    The Last Code

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import csv
+from itertools import zip_longest
 
-url =  "https://www.dell.com/en-us/shop/dell-laptops/scr/laptops"
+url =  "https://www.dell.com/en-us/shop/deals/business-deals/business-laptop-deals"
     
 laptop_names_dataset = []
 laptops_prices_dataset = []
-laptop_specs = []
-
+laptop_specs_dataset = []
+laptops_imgs_dataset = []
 response = requests.get(url) 
 src = response.content
 # print(src)
 soup = BeautifulSoup(src,"lxml")
-laptop_len = soup.find_all("div",{"class":"ps-variant-title-container"})
+laptop_len = soup.find_all("div",{"class":"ps-system-title-container ps-title-container"})
 
 
-laptop_names = soup.find_all("div",{"class":"ps-variant-title-container"})
+laptop_names = soup.find_all("div",{"class":"ps-system-title-container ps-title-container"})
 
-laptop_price = soup.find_all("div",{"class":"ps-variant-price"})
+laptop_price = soup.find_all("div",{"class":"ps-dell-price ps-simplified"})
 
 laptop_specs_first = soup.find_all("li",{"class":"variant-specs-column variant-specs-first-column"})
 
 laptop_specs_second = soup.find_all("li",{"class":"variant-specs-column variant-specs-second-column"})
+
+laptop_allSpecs = soup.find_all("div",{"class":"iconography-feature-specs"})
+
+laptop_img = soup.find_all("div",{"class":"ps-image ps-product-image-with-high-resolution"})
 
 
 def get_laptop_name(laptopnames):
@@ -32,33 +38,42 @@ def get_laptop_name(laptopnames):
 
 def get_laptop_prices(price):
     for i in range(len(laptop_len)):
-        lap_price = price[i].find("span").text.strip()
+        lap_price = price[i].find_all("span")
         laptops_prices_dataset.append(lap_price)
 
-def get_laptop_specs(specs1,spec2):
-    for i in range(7):
-        lap_specs = specs1[i].find_all("div",{"spec-value"}).text.strip()
-        lable_specs = specs1[i].find_all("div",{"spec-label"}).text.strip()
-        # lap_specs2 = spec2[i].find("div",{"spec-value"})
-        # lable_specs2 = spec2[i].find("div",{"spec-lable"})
-        laptop_specs.append(f"{lable_specs} : {lap_specs}")
-        # laptop_specs.append(f"{lap_specs2} : {lable_specs2}")
-        laptop_specs.append("-----")
-
-# def get_laptop_specs(specs1,spec2):
-#     lap_specs = specs1[0].find("div",{"spec-value"}).text.strip()
-#     lable_specs = specs1[0].find("div",{"spec-label"}).text.strip()
-#     lap_specs2 = spec2[0].find("div",{"spec-value"})
-#     lable_specs2 = spec2[0].find("div",{"spec-lable"})
-#     laptop_specs.append(f"{lable_specs} : {lap_specs}")
-#     laptop_specs.append(f"{lap_specs2} : {lable_specs2}")
-#     laptop_specs.append("-----")
 
 
-# get_laptop_prices(laptop_price)
-# get_laptop_name(laptop_names)
-get_laptop_specs(laptop_specs_first,laptop_specs_second)
-print(laptop_specs)
+def get_laptop_specs(spec):
+    specs_section = soup.find('section', class_='ps-show-hide')
+    specs = []
+    for spec_container in specs_section.find_all('div', class_='short-specs ps-dds-svg-icon-container'):
+        spec_name = spec_container.find('span', class_='svg-icon').next_sibling.strip()
+        specs.append(f"{spec_name}")
+        laptop_specs_dataset.append(specs)
+
+def get_laptop_image(url):
+    for i in range(len(laptop_len)):
+        lap_img = url[i].find("img")
+        img_src = lap_img.get('src')
+        laptops_imgs_dataset.append(img_src)
+
+get_laptop_prices(laptop_price)
+get_laptop_name(laptop_names)
+get_laptop_specs(laptop_allSpecs)
+get_laptop_image(laptop_img)
+
+file_list = [laptop_names_dataset, laptops_prices_dataset,laptop_specs_dataset,laptops_imgs_dataset]
+exported_ = zip_longest(*file_list)
+
+
+with open('laptops_dataset.csv', 'w', newline='', encoding='utf-8') as f:
+    wr = csv.writer(f)
+    wr.writerow(["Name","Price", "Specs","Images"])
+    wr.writerows(exported_)
+
+# print(laptops_imgs_dataset)
+
+
 
 
 
